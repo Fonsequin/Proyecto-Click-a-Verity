8df6e89fef57f01cfb771084659ceb69cd68717d
susering <joakkosigma@gmail.com>
2026-09-10 16:58:26 -0300

    Esteando express segun la tarea

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+const logros = [
+{
+id: 1,
+nombre: "Hacer tu primer click",
+descripcion: "Hiciste tu primer click con Verity!",
+},
+{
+id: 2,
+nombre: "El inicio",
+descripcion: "Compraste tu primera mejora con Verity!",
+},
+{
+id: 3,
+nombre: "100 verities",
+descripcion: "Conseguiste 100 clicks!",
+},
+{
+id: 4,
+nombre: "500 verities",
+descripcion:"Conseguiste 500 clicks!",
+},
+{
+id: 5,
+nombre: "Click a Verity!",
+descripcion: "Llegaste al top 1 de la leaderboard!",
+}
+];
+
+app.get("/logros", (solicitud, respuesta) => {
+respuesta.json(logros);
+});
+
+app.get("/logros/:id", (solicitud, respuesta) => {
+
+const id = Number(solicitud.params.id);
+
+const logro = logros.find(logro => logro.id === id);
+
+if (!logro) {
+return respuesta.status(404).json({
+mensaje: "Logro no encontrado"
+});
+}
+
+respuesta.json(logro);
+});
+
+app.listen(3000, () => {
+console.log("Servidor corriendo en http://localhost:3000 ");
+});
+
